68d8dd90c501e1cd7b3505434ce9f0db2b261ad7
Mohamed-Elkatib <mayiagnigni@worldbank.org>
2023-04-19 20:25:57 +0100

    Some changes in files

--- inst/scripts/01_select_vars.R
+++ inst/scripts/01_select_vars.R
@@ -6,28 +6,48 @@ library(knitr)
 library(RStata)
 library(stats)
 library(haven)
-load("~/World Bank/GitHub_Projects/NamibiaPovMap/inst/data/cencus_dt.RData")
-load("~/World Bank/GitHub_Projects/NamibiaPovMap/inst/data/survey_dt.RData")
+library(pacman)
+library(here)
+load(here("inst/data/cencus_dt.RData"))
+load(here("inst/data/survey_dt.RData"))
 
-survey_dt$ea_id <- survey_dt$cluster
-survey_dt$lwel_PPP <- log(survey_dt$wel_PPP)
+
+candvar_list<-c("rururb" ,"wel_PPP",  "wall", "floor" ,"water14" ,
+                "agecat", "relathh9", "marital5", "radio", "television" , "computer", "cellphone" , "landphone" ,
+                "internet", "waterpipe", "piped", "cluster")
+
+candvar_list <- c("rururb", "wall", "floor", "agecat", "hhsize")
+
+survey_dt[,candvar_list] <- lapply(survey_dt[,candvar_list] ,as.numeric)
+survey_dt$ea_id<-survey_dt$cluster
+survey_dt$lwel_PPP<-log(survey_dt$wel_PPP)
+
+dummy_dt <- dummify(survey_dt$region_name)
+
+colnames(dummy_dt)[grepl("!karas", colnames(dummy_dt))] <- "karas"
+
+colnames(dummy_dt) <- gsub(" ", "", colnames(dummy_dt))
+
+colnames(dummy_dt) <- paste0("areadummy_", colnames(dummy_dt))
+
+survey_dt$hhweight <- survey_dt$wta_hh
+
+survey_dt <- cbind(survey_dt, dummy_dt)
 
 
-candvar_list<-c("rururb" ,"rooms" , "wall" , "roof",
-             "floor", "fuelcook", "fuelligh", "heatsource", "water14", "toilet14",
-              "relathh9", "agecat","marital5", "radio",
-             "television", "computer", "cellphone" , "landphone" ,"internet",
-             "literacy", "everattd", "educat5"  )
+
+
+#file.create("Rstata.log")
 
 nam_selvars_list <-
-  countrymodel_select_stata(dt = survey_dt[,c("lwel_PPP", "wta_hh",
-                                                                 "ea_id",
-                                                                 candvar_list)],
-                            xvars = candvar_list,
+  countrymodel_select_stata(dt = na.omit(survey_dt[,c("lwel_PPP", "hhweight",
+                                                      "ea_id", colnames(dummy_dt),
+                                                      candvar_list)]),
+                            xvars = c(candvar_list, colnames(dummy_dt)),
                             y = "lwel_PPP",
-                            weights = "wta_hh",
+                            weights = "hhweight",
                             selection = "BIC",
-                            stata_path = "C:/Program Files/Stata17/StataMP-64.exe",
+                            stata_path = "C:/Program Files/Stata17/StataMP-64",
                             stata_vnum = 17)
 
 
